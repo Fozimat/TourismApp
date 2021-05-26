247b5c3e908713ff9c5c492b67b69760adbbec8a
Fozimat <fozimata@gmail.com>
2021-05-26 17:31:10 +0700

    Deleted Unused import

--- app/src/main/java/com/fozimat/made/tourismapp/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/fozimat/made/tourismapp/favorite/FavoriteViewModel.kt
@@ -2,7 +2,6 @@ package com.fozimat.made.tourismapp.favorite
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import com.fozimat.made.tourismapp.core.data.TourismRepository
 import com.fozimat.made.tourismapp.core.domain.usecase.TourismUseCase
 
 class FavoriteViewModel(tourismUseCase: TourismUseCase) : ViewModel() {
